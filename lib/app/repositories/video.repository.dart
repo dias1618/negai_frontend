dd0f9ebfc18f55373d315b3fb6b52f0a0313bd8a
Gabriel Dias <dias.1618@gmail.com>
2020-08-03 21:09:15 -0300

    Recomendação de videos
    A partir da categoria e não repetindo videos já vistos

--- lib/app/repositories/video.repository.dart
+++ lib/app/repositories/video.repository.dart
@@ -2,17 +2,19 @@ import 'package:aquila_frontend_main/app/shared/manager-repositories/dio_reposit
 import 'package:aquila_frontend_main/app/shared/manager-repositories/parameter_repository.dart';
 import 'package:aquila_frontend_main/app/shared/manager-repositories/repository.dto.dart';
 import 'package:aquila_frontend_main/app/shared/manager-repositories/repository_manager.dart';
+import 'package:aquila_frontend_main/app/stores/usuario.store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class VideoRepository{
 
   RepositoryManager repositoryManager = Modular.get<DioRepositoryManager>();
+  UsuarioStore usuarioStore = Modular.get<UsuarioStore>();
 
   Future<RepositoryDto> getVideos() async{
     RepositoryDto repositoryDto = await repositoryManager.read(
       ParameterRepository(
         data: {
-          "path": "/videos"
+          "path": "/videos?idUsuario=${usuarioStore.usuario.id}"
         }
       )
     );
